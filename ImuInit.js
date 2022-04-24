5c9c8406022a326eeeb11e90f4fcb74ba3adf051
Aurelien Labate <aurelien@labate.me>
2022-04-24 19:51:59 +0200

    Fix init on firefox

--- ImuInit.js
+++ ImuInit.js
@@ -2,12 +2,23 @@
  * Init function from the main web page js scope
  */
 function ImuInit() {
-    console.log('[IMU] ImuInit started')
     // Build the UI once page loaded
-    window.addEventListener('load', () => {
-        ImuGui.addDrawToolBoxButtons()
-        ImuGui.initStyleTweaking()
-        ImuGui.initInteractionTweaking()
-    })
+    const onload = (retryCount = 0) => {
+        // Retry later if ImuGui is not yet defined
+        if (typeof ImuGui === 'undefined') {
+            if (retryCount > 50) {
+                throw 'Failed to load the ImuGui global object'
+            }
+            setTimeout(() => onload(retryCount + 1), 100)
+        }
+        else {
+            console.log('[IMU] ImuInit started with', retryCount, 'tries')
+            ImuGui.addDrawToolBoxButtons()
+            ImuGui.initStyleTweaking()
+            ImuGui.initInteractionTweaking()
+        }
+    }
+    onload()
 }
+console.log('[IMU] Calling ImuInit')
 ImuInit()
